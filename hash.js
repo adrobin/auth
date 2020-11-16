afc79915520b7954cd853d7c53f9d92012d4ac5a
Rabin <rabin.saedi@gmail.com>
2020-11-16 14:09:32 +0545

    First COmmit

---
+++ hash.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt');
+const { MD5 } = require('crypto-js');
+const jwt = require('jsonwebtoken');
+// bcrypt.genSalt(10,(err,salt)=>{
+//     if(err) return next(err)
+//     console.log(salt)
+//     bcrypt.hash('password',salt,(err,hash)=>{
+//         if(err) return next(err)
+//         console.log(hash)
+//     })
+//     console.log("Hello")
+// })
+
+const id = '1000';
+const secret = "supersecret";
+const receivedToken = 'eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y';
+const token = jwt.sign(id,secret);
+const decodeToken = jwt.verify(receivedToken,secret)
+console.log(decodeToken)
